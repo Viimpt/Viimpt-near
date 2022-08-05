8c23e72eaa8dbd243dfd6d5c584e4cd86aff99c9
esmeii <eunseong@ewhain.net>
2022-08-05 21:02:44 +0900

    esmeii:add src folder for login template

---
+++ Viimpt-near/frontend/src/ModalBack.js
@@ -0,0 +1,8 @@
+import React,{Component} from "react"
+class ModalBack extends React.Component {
+    render() {
+      return <button className="bringitback" onClick={ this.props.onClick } key={ this.props.className }>Brind the modal back !</button>
+    }
+  }
+
+export default ModalBack;
