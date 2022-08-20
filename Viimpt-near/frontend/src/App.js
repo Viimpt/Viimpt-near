63c9874ae17a98ab163c828865b744ac1a8929aa
esmeii <eunseong@ewhain.net>
2022-08-20 19:49:01 +0900

    esme: FINALLY HOME/login page

--- Viimpt-near/frontend/src/App.js
+++ Viimpt-near/frontend/src/App.js
@@ -1,32 +1,22 @@
-import React, {Component} from "react";
-import {connect} from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
+import PostListPage from './pages/PostListPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import WritePage from './pages/WritePage';
+import PostPage from './pages/PostPage';
 
-
-
-import ReactstrapNavbar from "./R049_ReactstrapNavbar";
-import ReactstrapAlerts from './R034_ReactstrapAlerts';
-import ReactstrapBreadcrumbs from "./R036_ReactstrapBreadcrumbs";
-import ReactstrapCarousel from "./R041_ReactstrapCarousel";
-import 'bootstrap/dist/css/bootstrap.css'
-import { render } from "react-dom";
-
-class App extends Component {
-    render(){
-        return(
-            <div>
-                <ReactstrapNavbar />
-                < ReactstrapCarousel/>
-                <ReactstrapBreadcrumbs />
-                
-            </div>
-        )
-    }
-}
-// let mapStateToProps = (state, props) => {
-//     console.log('Props from index.js' + props.indexProp)
-//     return {
-//         str: state.data.str,
-//     };
-// };
-// App = connect(mapStateToProps, null)(App);
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<PostListPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/write" element={<WritePage />} />
+      <Route path="/@:username">
+        <Route index element={<PostListPage />} />
+        <Route path=":postId" element={<PostPage />} />
+      </Route>
+    </Routes>
+  );
+};
 export default App;
