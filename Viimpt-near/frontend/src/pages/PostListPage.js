1c59716f9837efb736856c02dc69f12016f4a681
esmeii <eunseong@ewhain.net>
2022-08-26 14:19:00 +0900

    edit:Home News API

--- Viimpt-near/frontend/src/pages/PostListPage.js
+++ Viimpt-near/frontend/src/pages/PostListPage.js
@@ -1,10 +1,27 @@
-import React from 'react';
-import HeaderContainer from '../containers/common/HeaderContainer';
+import React, { useState } from "react";
+import HeaderContainer from "../containers/common/HeaderContainer";
+import axios from "axios";
+import NewsList from "../components/common/NewsList";
+
+//95b8d71da2cc44268cde62d495c1e41b
 const PostListPage = () => {
+  // const [data, setData] = useState(null);
+  // const onClick = async () => {
+  //   try {
+  //     const response = await axios.get(
+  //       "https://newsapi.org/v2/top-headlines?country=kr&category=entertainment&apiKey=95b8d71da2cc44268cde62d495c1e41b"
+  //     );
+  //     setData(response.data);
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // };
   return (
     <>
       <HeaderContainer />
-      <div>안녕하세요.</div>
+      <div>
+        <NewsList />
+      </div>
     </>
   );
 };
