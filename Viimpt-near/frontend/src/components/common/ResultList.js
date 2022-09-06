03751a24bd08fa69950dd11919119d700d9c29ec
esmeii <eunseong@ewhain.net>
2022-09-06 11:58:51 +0900

    edit:add some text

--- Viimpt-near/frontend/src/components/common/ResultList.js
+++ Viimpt-near/frontend/src/components/common/ResultList.js
@@ -20,21 +20,21 @@ function ResultList() {
         <tbody>
           <tr>
             <td>1</td>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-            <Badge />
+            <td>아이브</td>
+            <td>383838</td>
+            <td>2002390</td>
           </tr>
           <tr>
             <td>2</td>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
+            <td>르세라핌</td>
+            <td>113933</td>
+            <td>2002300</td>
           </tr>
           <tr>
             <td>3</td>
-            <td colSpan={2}>Larry the Bird</td>
-            <td>@twitter</td>
+            <td>뉴진스</td>
+            <td>39400</td>
+            <td>199999</td>
           </tr>
         </tbody>
       </Table>
@@ -53,24 +53,59 @@ function ResultList() {
         <tbody>
           <tr>
             <td>1</td>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-            <Badge />
+            <td>아이브</td>
+            <td>383838</td>
+            <td>2002390</td>
           </tr>
           <tr>
             <td>2</td>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
+            <td>르세라핌</td>
+            <td>113933</td>
+            <td>2002300</td>
           </tr>
           <tr>
             <td>3</td>
-            <td colSpan={2}>Larry the Bird</td>
-            <td>@twitter</td>
+            <td>뉴진스</td>
+            <td>39400</td>
+            <td>199999</td>
           </tr>
         </tbody>
       </Table>
+
+      <h2>인기가요</h2>
+      <ListGrouping />
+
+      <Table striped bordered hover size="sm">
+        <thead>
+          <tr>
+            <th>순위</th>
+            <th>가수명</th>
+            <th>득표 수</th>
+            <th>총점</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>아이브</td>
+            <td>383838</td>
+            <td>2002390</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>르세라핌</td>
+            <td>113933</td>
+            <td>2002300</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>뉴진스</td>
+            <td>39400</td>
+            <td>199999</td>
+          </tr>
+        </tbody>
+      </Table>
+
       <h2>음악중심</h2>
       <ListGrouping />
 
@@ -86,21 +121,21 @@ function ResultList() {
         <tbody>
           <tr>
             <td>1</td>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-            <Badge />
+            <td>아이브</td>
+            <td>383838</td>
+            <td>2002390</td>
           </tr>
           <tr>
             <td>2</td>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
+            <td>르세라핌</td>
+            <td>113933</td>
+            <td>2002300</td>
           </tr>
           <tr>
             <td>3</td>
-            <td colSpan={2}>Larry the Bird</td>
-            <td>@twitter</td>
+            <td>뉴진스</td>
+            <td>39400</td>
+            <td>199999</td>
           </tr>
         </tbody>
       </Table>
