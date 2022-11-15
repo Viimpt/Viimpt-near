04371d3651ee1e1e9ec041359e1ca77096e00a3b
Gom3rye <kimmyeongji3@gmail.com>
2022-11-15 21:35:03 +0900

    backend init

---
+++ Viimpt-near/backend/viimpt/src/main/java/com/viimpt/vote/viimpt/ViimptApplication.java
@@ -0,0 +1,13 @@
+package com.viimpt.vote.viimpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViimptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViimptApplication.class, args);
+	}
+
+}
---
+++ Viimpt-near/backend/viimpt/src/test/java/com/viimpt/vote/viimpt/ViimptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viimpt.vote.viimpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViimptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
